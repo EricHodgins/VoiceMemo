044cbd7c0c6e12d6989bc0939b924196d983e044
Eric Hodgins <hodgins.e@gmail.com>
2016-10-22 01:32:25 -0400

    feat: Collecting data updates.

--- VoiceMemo/DataProvider.swift
+++ VoiceMemo/DataProvider.swift
@@ -7,10 +7,23 @@
 //
 
 import Foundation
+import CloudKit
+
+protocol DataProviderDelegate: class {
+    func processUpdates(updates: [DataProviderUpdate<Memo>])
+    func providerFailed(error: MemoErrorType)
+}
+
+enum DataProviderUpdate<T> {
+    case Insert(T)
+}
 
 class DataProvider {
     
     let manager = CloudPersistenceManager()
+    var updates = [DataProviderUpdate<Memo>]()
+    
+    private weak var delegate: DataProviderDelegate?
     
     func performQuery(type: QueryType) {
         manager.perform(query: type.query) { (result) in
@@ -18,8 +31,60 @@ class DataProvider {
         }
     }
     
+    func fetch(recordID: CKRecordID) {
+        manager.fetch(recordID: recordID) { (result) in
+            self.processResult(result: result)
+        }
+    }
+    
+    func save(memo: Memo) {
+        manager.save(memo: memo) { (result) in
+            self.processResult(result: result)
+        }
+    }
+    
     private func processResult(result: Result<[Memo]>) {
-        
+        DispatchQueue.main.async {
+            switch result {
+            case .Success(let memos):
+                self.updates = memos.map { DataProviderUpdate.Insert($0) }
+                self.delegate?.processUpdates(updates: self.updates)
+            case .Failure(let error):
+                self.delegate?.providerFailed(error: error)
+            }
+        }
+    }
+    
+    private func processResult(result: Result<Memo>) {
+        DispatchQueue.main.async {
+            switch result {
+            case .Success(let memo):
+                self.updates = [DataProviderUpdate.Insert(memo)]
+                self.delegate?.processUpdates(updates: self.updates)
+            case .Failure(let error):
+                self.delegate?.providerFailed(error: error)
+            }
+        }
     }
     
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
